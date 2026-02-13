320da99ecd9629059ee75f89f9226c19f359bf23
tekkons <18157396+tekkons@users.noreply.github.com>
2026-02-13 15:46:47 +0200

    Update demo module

--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
 locals {
+  tags = ["cloud-init", "terraform"]
   ssh_keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIP0DMQ..."
   ]
--- main.tf
+++ main.tf
@@ -1,36 +1,25 @@
 module "demo" {
   source = "./modules/vm"
 
-  vm_id    = 100
-  hostname = "demo"
+  name  = "demo"
+  vm_id = 100
+  tags  = concat(["demo", "debian13"], local.tags)
 
   ssh_keys = local.ssh_keys
   packages = local.packages
 
-  description = <<-EOT
-    "Managed by Terraform"
-  EOT
-
-  tags = ["demo", "debian13", "cloud-init", "terraform"]
-
-  cpu_cores = 1
-  memory    = 1024
-
-  disk = {
-    file_id   = module.images.image_ids["debian13"]
-    interface = "scsi0"
-    discard   = "on"
-    iothread  = "true"
-    size      = 16
-    ssd       = true
-    cache     = "writeback"
-  }
-
+  cpu_cores    = 1
+  memory       = 1024
   ipv4_address = "192.168.1.10/24"
   ipv4_gateway = "192.168.1.1"
 
   dns = {
     "domain"  = "example.com"
     "servers" = ["1.1.1.1", "8.8.8.8"]
+  }
+
+  disk = {
+    file_id = module.images.image_ids["debian13"]
+    size    = 16
   }
 }
